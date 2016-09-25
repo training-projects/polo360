ce7c91742e453b5e70f12dd30460ee9b03309457
theandersonn <theandersonn@gmail.com>
2016-09-25 19:04:58 -0300

    Rearranges class css

--- gulp/tasks/concatjs.js
+++ gulp/tasks/concatjs.js
@@ -16,21 +16,21 @@ gulp.task('concatjs', function() {
         '../assets/src/js/libs/jquery-2.2.4.js',
         
         // BOOTSTRAP    
-        '../assets/src/js/bootstrap/affix.js',
-        '../assets/src/js/bootstrap/alert.js',
-        '../assets/src/js/bootstrap/button.js',
-        //'../assets/src/js/bootstrap/carousel.js',
-        '../assets/src/js/bootstrap/collapse.js',
-        '../assets/src/js/bootstrap/dropdown.js',
-        '../assets/src/js/bootstrap/modal.js',
+        // '../assets/src/js/bootstrap/affix.js',
+        // '../assets/src/js/bootstrap/alert.js',
+        // '../assets/src/js/bootstrap/button.js',
+        // '../assets/src/js/bootstrap/carousel.js',
+        // '../assets/src/js/bootstrap/collapse.js',
+        // '../assets/src/js/bootstrap/dropdown.js',
+        // '../assets/src/js/bootstrap/modal.js',
         // '../assets/src/js/bootstrap/popover.js', Esse sapeca está com treta :(
-        '../assets/src/js/bootstrap/scrollspy.js',
-        '../assets/src/js/bootstrap/tab.js',
-        '../assets/src/js/bootstrap/tooltip.js',
-        '../assets/src/js/bootstrap/transition.js',
+        // '../assets/src/js/bootstrap/scrollspy.js',
+        // '../assets/src/js/bootstrap/tab.js',
+        // '../assets/src/js/bootstrap/tooltip.js',
+        //'../assets/src/js/bootstrap/transition.js',
 
         // FANCYBOX
-        '../assets/src/js/fancybox/jquery.fancybox.js',        
+        // '../assets/src/js/fancybox/jquery.fancybox.js',        
 
         // OWL CAROUSEL
         '../assets/src/js/owl-carousel/owl.carousel.js',
